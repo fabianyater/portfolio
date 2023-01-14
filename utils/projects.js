9995912492edafde52501e711045ae1f851b041a
Fabian Yate <fabianyate31@gmail.com>
2023-01-13 22:18:17 -0500

    Add projects data

---
+++ utils/projects.js
@@ -0,0 +1,144 @@
+import github from '../images/icons/github.svg'
+import preview from '../images/icons/preview.svg'
+import linktree from '../images/linktree-project.png'
+import weather from '../images/weather-project.png'
+import pokedex from '../images/pokedex-project.png'
+
+export const projects = [
+  {
+    title: "Linktree Clone",
+    description: "A clone of the popular linktree website.",
+    image: linktree,
+    links: [
+      {
+        name: "Github",
+        icon: github,
+        href: "https://github.com/fabianyater/clon-linktree",
+      },
+      {
+        name: "Live",
+        icon: preview,
+        href: "https://clon-linktree.vercel.app/",
+      },
+    ],
+  },
+  {
+    title: "Weather App",
+    description: "A weather app made for Midudev hackaton.",
+    image: weather,
+    links: [
+      {
+        name: "Github",
+        icon: github,
+        href: "https://github.com/fabianyater/hackaton-weather-app",
+      },
+      {
+        name: "Live",
+        icon: preview,
+        href: "https://fyrdev-weatherapp.netlify.app/",
+      },
+    ],
+  },
+  {
+    title: "Pokédex",
+    description: "Static page to show pokémon information.",
+    image: pokedex,
+    links: [
+      {
+        name: "Github",
+        icon: github,
+        href: "https://github.com/fabianyater/Pokedex",
+      },
+      {
+        name: "Live",
+        icon: preview,
+        href: "https://fabianyater.github.io/Pokedex/",
+      },
+    ],
+  },
+  {
+    title: "Pokédex",
+    description: "Static page to show pokémon information.",
+    image: pokedex,
+    links: [
+      {
+        name: "Github",
+        icon: github,
+        href: "https://github.com/fabianyater/Pokedex",
+      },
+      {
+        name: "Live",
+        icon: preview,
+        href: "https://fabianyater.github.io/Pokedex/",
+      },
+    ],
+  },
+  {
+    title: "Pokédex",
+    description: "Static page to show pokémon information.",
+    image: pokedex,
+    links: [
+      {
+        name: "Github",
+        icon: github,
+        href: "https://github.com/fabianyater/Pokedex",
+      },
+      {
+        name: "Live",
+        icon: preview,
+        href: "https://fabianyater.github.io/Pokedex/",
+      },
+    ],
+  },
+  {
+    title: "Pokédex",
+    description: "Static page to show pokémon information.",
+    image: pokedex,
+    links: [
+      {
+        name: "Github",
+        icon: github,
+        href: "https://github.com/fabianyater/Pokedex",
+      },
+      {
+        name: "Live",
+        icon: preview,
+        href: "https://fabianyater.github.io/Pokedex/",
+      },
+    ],
+  },
+  {
+    title: "Pokédex",
+    description: "Static page to show pokémon information.",
+    image: pokedex,
+    links: [
+      {
+        name: "Github",
+        icon: github,
+        href: "https://github.com/fabianyater/Pokedex",
+      },
+      {
+        name: "Live",
+        icon: preview,
+        href: "https://fabianyater.github.io/Pokedex/",
+      },
+    ],
+  },
+  {
+    title: "Pokédex",
+    description: "Static page to show pokémon information.",
+    image: pokedex,
+    links: [
+      {
+        name: "Github",
+        icon: github,
+        href: "https://github.com/fabianyater/Pokedex",
+      },
+      {
+        name: "Live",
+        icon: preview,
+        href: "https://fabianyater.github.io/Pokedex/",
+      },
+    ],
+  },
+]
