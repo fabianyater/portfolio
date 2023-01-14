4ba13fa85d1854b0963cd513ba83884f21f3b9ea
Fabian Yate <fabianyate31@gmail.com>
2023-01-13 20:09:36 -0500

    Add icons data

---
+++ utils/icons.js
@@ -0,0 +1,89 @@
+import html from '../images/icons/html.svg'
+import css from '../images/icons/css.svg'
+import javascript from '../images/icons/javascript.svg'
+import react from '../images/icons/react.svg'
+import tailwind from '../images/icons/tailwind.svg'
+import vscode from '../images/icons/vscode.svg'
+import git from '../images/icons/git.svg'
+import github from '../images/icons/github.svg'
+import java from '../images/icons/java.svg'
+import spring from '../images/icons/spring.svg'
+import springboot from '../images/icons/springboot.svg'
+import postgres from '../images/icons/postgresql.svg'
+import postman from '../images/icons/postman.svg'
+import intellij from '../images/icons/intellij.svg'
+import docker from '../images/icons/docker.svg'
+import azure from '../images/icons/azure.svg'
+import openapi from '../images/icons/openapi.svg'
+
+export const icons = [
+  {
+    name: 'Html',
+    icon: html
+  },
+  {
+    name: 'Css',
+    icon: css
+  },
+  {
+    name: 'Javascript',
+    icon: javascript
+  },
+  {
+    name: 'React',
+    icon: react
+  },
+  {
+    name: 'Tailwind',
+    icon: tailwind
+  },
+  {
+    name: 'Vscode',
+    icon: vscode
+  },
+  {
+    name: 'Git',
+    icon: git
+  },
+  {
+    name: 'Github',
+    icon: github
+  },
+  {
+    name: 'Java',
+    icon: java
+  },
+  {
+    name: 'Spring',
+    icon: spring
+  },
+  {
+    name: 'Springboot',
+    icon: springboot
+  },
+  {
+    name: 'Postgres',
+    icon: postgres
+  },
+  {
+    name: 'Postman',
+    icon: postman
+  },
+  {
+    name: 'Intellij',
+    icon: intellij
+  },
+  {
+    name: 'Docker',
+    icon: docker
+  },
+  {
+    name: 'Azure',
+    icon: azure
+  },
+  {
+    name: 'Openapi',
+    icon: openapi
+  }
+]
+
