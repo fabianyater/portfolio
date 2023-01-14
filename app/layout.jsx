d95be423e36e218a4d10f44c87918de186115dc4
Fabian Yate <fabianyate31@gmail.com>
2023-01-14 00:08:44 -0500

    Remove height style class

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,7 +4,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="es" className='scroll-smooth'>
       <head />
-      <body className="h-screen bg-gradient-to-b from-secondary via-tertiary to-quaternary">{children}</body>
+      <body className="bg-gradient-to-b from-secondary via-tertiary to-quaternary">{children}</body>
     </html>
   )
 }
