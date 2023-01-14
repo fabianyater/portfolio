c920c7b37a81efea9695478ef33e2687e0abeaa3
Fabian Yate <fabianyate31@gmail.com>
2023-01-13 20:11:23 -0500

    Add scroll mooth in Html tag

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,7 +2,7 @@ import './globals.css'
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="es">
+    <html lang="es" className='scroll-smooth'>
       <head />
       <body className="h-screen bg-gradient-to-b from-secondary via-tertiary to-quaternary">{children}</body>
     </html>
