9d468dc7eb2480a8b18630958c42d4143c315d2f
Fabian Yate <fabianyate31@gmail.com>
2023-01-13 20:09:02 -0500

    add menu data

---
+++ utils/menu.js
@@ -0,0 +1,18 @@
+export const menu = [
+  {
+    name: "Home",
+    href: "#home",
+  },
+  {
+    name: "Skills",
+    href: "#skills",
+  },
+  {
+    name: "Projects",
+    href: "#projects",
+  },
+  {
+    name: "Contact",
+    href: "#contact",
+  }
+];
