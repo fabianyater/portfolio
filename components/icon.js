370d4800b4731f29e76795adf42c3a421679d620
Fabian Yate <fabianyate31@gmail.com>
2023-01-15 20:41:03 -0500

    Update css styles

--- components/icon.js
+++ components/icon.js
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function Icon ( {image, name} ) {
   return(
-    <div className="flex flex-col gap-1 items-center justify-center">
+    <div className="flex flex-col gap-1 items-center justify-center w-6 h-6 sm:w-full">
       <Image src={image} alt={name} className="transition-all hover:scale-125" />
-      <p>{name}</p>
+      <p className="text-xs sm:font-medium sm:text-base">{name}</p>
     </div>
   )
 }
